7186c02c095c7ecb3d07f6bd4953e98fcc619fbd
Marco Andronaco <andronacomarco@gmail.com>
2026-03-21 23:23:59 +0100

    Change walls color

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const CENTER = Math.floor(GRID_SIZE / 2);
 export const PLACEMENT_COOLDOWN = 5; // seconds
 
 export const COLORS = {
-  WALL: '#7A9E7E',
+  WALL: '#D4AFCD',
   FLOOR: '#E2C290',
   BACKGROUND: '#0B0033',
   DOOR_BORDER: '#D4AFCD',
